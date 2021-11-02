9e58109cd38f2fa789b753816ecf3811b5fc4fa7
TinaafWeb <88359480+TinaafWeb@users.noreply.github.com>
2021-11-01 23:32:44 -0400

    Add project files.

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolCommunityContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+
+    }
+}
